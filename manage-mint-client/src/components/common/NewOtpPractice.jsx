fcef4fb1bdabbc1cc38795c17ccc3253571d53ee
Fuzail Vhora <55022788+fuzavhora@users.noreply.github.com>
2025-04-11 08:27:48 +0530

    Added NewOtpPract.jsx for the optimization

---
+++ manage-mint-client/src/components/common/NewOtpPractice.jsx
@@ -0,0 +1,10 @@
+import {useState} from "react"
+const NewOtpPract = () => {
+const otpCount = 6
+const [] = useState(new Array(otpCount).fill(""))
+return (
+   <div>
+       <h1>OTP VERIFICATION</h1>
+   </div>
+)
+}
